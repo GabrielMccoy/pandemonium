cd7f40252e4be540de3da8fb289f54d200c06a2e
GabrielMccoy <gmcc0022@student.monash.edu>
2025-10-21 18:49:34 +1100

    coordinate documentation

--- R/coordinates.R
+++ R/coordinates.R
@@ -90,6 +90,15 @@ normCoords <- function(df, ...){
 #' Returns the input data frame. This is used when other coordinate computations fail.
 #' In general, scaling of the inputs is recommended before clustering.
 #'
+#' Externally calculated coordinates can be used through userCoords or
+#' as input data with rawCoords used as the coordinate function.
+#' The use of userCoords over rawCoords is in the treatment of input data.
+#' As pandemonium displays the input data in many plots the use of coordinates
+#' as input data will result in these plots being less meaningful for interpretation.
+#' Use userCoords where coordinates are necessary to calculate distances but
+#' interpretation from plots of clustering space is necessary.
+#'
+#'
 #' @param df data frame
 #' @param ... other expected values of getCoords
 #'
@@ -109,6 +118,14 @@ rawCoords <- function(df, ...){
 #'
 #' Allows the use of externally calculated coordinates in the app.
 #' Can only be used when variables are not reassigned between the two spaces.
+#'
+#' Externally calculated coordinates can be used through userCoords or
+#' as input data with rawCoords used as the coordinate function.
+#' The use of userCoords over rawCoords is in the treatment of input data.
+#' As pandemonium displays the input data in many plots the use of coordinates
+#' as input data will result in these plots being less meaningful for interpretation.
+#' Use userCoords where coordinates are necessary to calculate distances but
+#' interpretation from plots of clustering space is necessary.
 #'
 #' @param user_coords coordinate matrix the size of the space it will be used on
 #'
